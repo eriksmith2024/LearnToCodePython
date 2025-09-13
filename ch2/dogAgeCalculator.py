7af793a589142d4cc9175b647585c295e07d2bd8
Erik Smith <eriksmith.career@hotmail.com>
2025-09-13 19:02:13 +0100

    updates

--- ch2/dogAgeCalculator.py
+++ ch2/dogAgeCalculator.py
@@ -14,9 +14,9 @@ dog_name = input("What is your dog's name? ")
 
 dog_age = int(input("What is your dog's age? "))
 
-human_age = dog_age * 7
+human_age = int(dog_age * 7)
 
-print(int(human_age))
+print(human_age)
 
 
 
