5152a29b3df42fb99edc38e4d48ce83eb074963c
Erik Smith <eriksmith.career@hotmail.com>
2025-09-19 15:10:32 +0100

    ch4 for loop & range fix

--- ch4/codemagnetsForInLoop.py
+++ ch4/codemagnetsForInLoop.py
@@ -1,11 +1,13 @@
+
+# FOR IN LOOP VERSION
 smoothies = [ 'cocunut', 'strawberry', 'banana', 'tropical', 'acai berry']
 has_coconut = [True, False, False, True, False]
 
+length = len(has_coconut)
 print('')
-i = 0
-while i < len(has_coconut):
+
+for i in range(length):
     if has_coconut[i]:
         print(smoothies[i], 'contains coconut')
-    i = i + 1
 
 print('')
--- ch4/tempCodeRunnerFile.py
+++ ch4/tempCodeRunnerFile.py
@@ -1,2 +1,11 @@
-length = length * -1
-i = -2
+smoothies = [ 'cocunut', 'strawberry', 'banana', 'tropical', 'acai berry']
+has_cocunut = [True, False, False, True, False]
+
+print('')
+i = 0
+while i < len(has_cocunut):
+    if has_cocunut[i]:
+        print(smoothies[i], 'contains cocunut')
+    i = i + 1
+
+print('')
