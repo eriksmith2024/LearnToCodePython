1ad9b027e67d8d8fbc95cf5b3606e280bb9d4118
Erik Smith <eriksmith.career@hotmail.com>
2025-09-19 14:11:57 +0100

    ch4 smoothies list

--- ch4/bubbleScores.py
+++ ch4/bubbleScores.py
@@ -9,3 +9,20 @@ scores = [60, 50, 60, 58, 54, 54,
           69, 64, 66, 55, 52, 61,
           46, 31, 57, 52, 44, 18,
           41, 53, 55, 61, 51, 44]
+
+
+i = 0
+length = len(scores)
+print('\nTotal Bubble tests:',length,'\n')
+
+# This solution prints the code with a space between the # and the index.  
+# while i < length:
+#     print('Bubble solution #', i, "scored: ", scores[i])
+#     i = i + 1
+# To solve the gap issue you cant concatante the i as it is a number you need to make it string first to concatanate
+while i < length:
+    print('Bubble solution #' + str(i), "scored: ", scores[i])
+    i = i + 1
+
+
+
