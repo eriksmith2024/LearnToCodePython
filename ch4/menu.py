ab929cf593c2193aa115948950824df9d183a9d3
Erik Smith <eriksmith.career@hotmail.com>
2025-09-19 15:54:54 +0100

    ch4 mystery fix multiplying a list

--- ch4/menu.py
+++ ch4/menu.py
@@ -4,4 +4,23 @@ menu.append('Sushi')
 
 print('\n', menu, '\n')
 
+del menu[0]
+print(menu, '\n')
 
+
+menu.extend(['BBQ', 'Tacos'])
+print('\n', menu, '\n')
+
+menu.append(['Burger', 'Ribs'])
+print('\n', menu, '\n')
+
+# append = add one item to the list even if that is multiple items in a list they will be added as one item
+# extend = add many things to the list that are then seperate items that can be iterated over  
+
+
+# below contatenation of two lists does the same as extend
+menu = menu + ['Pizza', 'Fries']
+print('\n', menu, '\n') 
+
+menu.insert(1, 'Stir Fry')
+print('\n', menu, '\n')
