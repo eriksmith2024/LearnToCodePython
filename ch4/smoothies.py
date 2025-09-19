51139161b1841f7c204aadad49760eeec0d5eff9
Erik Smith <eriksmith.career@hotmail.com>
2025-09-19 13:20:24 +0100

    ch4 updates

--- ch4/smoothies.py
+++ ch4/smoothies.py
@@ -1,3 +1,19 @@
 smoothies = ['Coconut', 'Stawberry', 'Banana', 'Pineapple', 'Acai Berry']
+print(smoothies)
 
 favourite = smoothies[2]
+
+smoothies[3] = 'Tropical'
+print(smoothies)
+
+length = len(smoothies)
+print(length)
+
+length = len(smoothies)
+last = smoothies[length -1]
+second_last = smoothies[length -2]
+third_last = smoothies[length -3]
+print(last)
+print(second_last)
+print(third_last)
+
