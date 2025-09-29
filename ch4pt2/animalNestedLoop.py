436a2724fe91e1a95ec9f037eade200720f9b73e
Erik Smith <eriksmith.career@hotmail.com>
2025-09-29 14:43:40 +0100

    ch4 Part 2 animal nested loop

--- ch4pt2/animalNestedLoop.py
+++ ch4pt2/animalNestedLoop.py
@@ -4,4 +4,9 @@ for word in ['ox', 'cat', 'lion', 'tiger', 'bobcat']:
         if(letters % i) == 0:
             print(i, word)
 
-            
+
+# Basically goes the loop of words 
+# and then an inner loop for each word loops through index of 2 to 7
+# it assigns the word length of the word i.e. ox is 2
+# it then divides the number against each of the index, 2,3,4,5,6
+# if the length divided by the index number = 0 it gets printed
