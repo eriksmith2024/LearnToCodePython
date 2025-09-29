9a28ddc1257498c58c9659ac048babbcfb0f6a11
Erik Smith <eriksmith.career@hotmail.com>
2025-09-29 14:28:34 +0100

    ch4 Part 2

---
+++ ch4pt2/imultipliedjNestLoop.py
@@ -0,0 +1,8 @@
+for i in range(0,4):
+    for j in range(0,4):
+        print(i * j)
+
+# Basically for each of the i loops it loops through 4 times on multiplied by the j loop
+# After it loops through LOOP ONE i(0)*j(0) = 0  LOOP TWO i(0) * j(1) = 0 LOOP THREE i(0) * j(2)= 0 LOOP FOUR i(0) * j(3) = 0
+# It then proceeds to next loop of i  LOOP ONE i(1) * j(0) = 0 LOOP TWO i(1) * j(1) = 1 LOOP THREE  i(1) * j(2) = 2 LOOP FOUR i(1) * j(3) = 3
+# IT CONTINUED through i = (2) then i =(3) multiplying i by j(0) j(1) j(2) j(3) in each i loop
