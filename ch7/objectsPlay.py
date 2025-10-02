f053e1039d5822996519a35b78343edc66164d72
Erik Smith <eriksmith.career@hotmail.com>
2025-10-02 12:33:13 +0100

    ch7 turtle race setup

---
+++ ch7/objectsPlay.py
@@ -0,0 +1,13 @@
+my_list = list()
+my_list.append('first')
+my_list.append('second')
+my_list.reverse()
+print(my_list)
+
+greeting = str('hello reader')
+shout = greeting.upper()
+print(shout)
+
+pi = float(3.1415)
+is_int = pi.is_integer()
+print(pi, is_int)
