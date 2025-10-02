0a87db58518f3bac43528e5d6a9506a846666a3a
Erik Smith <eriksmith.career@hotmail.com>
2025-10-02 11:07:53 +0100

    ch7 turtle update

--- ch7/turtle_test.py
+++ ch7/turtle_test.py
@@ -2,14 +2,19 @@ import turtle
 
 slowspoke = turtle.Turtle()
 slowspoke.shape('turtle')
+slowspoke.color('blue')
 
-slowspoke.forward(100)
-slowspoke.right(90)
-slowspoke.forward(100)
-slowspoke.right(90)
-slowspoke.forward(100)
-slowspoke.right(90)
-slowspoke.forward(100)
-slowspoke.right(90)
+pokey = turtle.Turtle()
+pokey.shape('turtle')
+pokey.color('red')
+
+def make_sqaure(the_turtle):
+    for i in range(0,4):
+        the_turtle.forward(100)
+        the_turtle.right(90)
+
+make_sqaure(slowspoke)
+pokey.right(45)
+make_sqaure(pokey)
 
 turtle.done()
