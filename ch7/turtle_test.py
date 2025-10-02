5793ff23b9a64b4e2f398e65757b35018c36b8e2
Erik Smith <eriksmith.career@hotmail.com>
2025-10-02 11:19:26 +0100

    ch7 turtle spiral

--- ch7/turtle_test.py
+++ ch7/turtle_test.py
@@ -13,8 +13,18 @@ def make_sqaure(the_turtle):
         the_turtle.forward(100)
         the_turtle.right(90)
 
-make_sqaure(slowspoke)
-pokey.right(45)
-make_sqaure(pokey)
+def make_spiral(the_turtle):
+    for i in range (0,36):
+        make_sqaure(the_turtle)
+        the_turtle.right(10)
+
+# make_sqaure(slowspoke)
+# pokey.right(45)
+# make_sqaure(pokey)
+
+make_spiral(slowspoke)
+pokey.right(5)
+make_spiral(pokey)
+
 
 turtle.done()
