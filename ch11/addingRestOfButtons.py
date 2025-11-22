2797cb8b70989ab015aff13f62fc9a357af8fd38
Erik Smith <eriksmith.career@hotmail.com>
2025-11-22 14:39:14 +0000

    update

--- ch11/addingRestOfButtons.py
+++ ch11/addingRestOfButtons.py
@@ -28,8 +28,34 @@ start_button.grid(row=1, column=0, sticky=W, padx=20, pady=20)
 option.grid(row=1,column=1,padx=20)
 clear_button.grid(row=1, column=2, sticky=E, pady=20)
 
+def update():
+    global grid_view
+
+    grid_view.delete(ALL)
+
+    model.next_gen()
+    for i in range(0, model.height):
+        for j in range(0, model.width):
+            if model.grid_model[i][j] == 1:
+                draw_cell(i, j, 'black')
+
+def draw_cell(row, col, color):
+    global grid_view, cell_size
+
+    if color == 'black':
+        outline = 'grey'
+    else:
+        outline = 'white'
+
+    grid_view.create_rectangle(row*cell_size,
+                               col*cell_size,
+                               row*cell_size+cell_size,
+                               col*cell_size+cell_size,
+                               fill=color, outline=outline)
+
 if __name__ == '__main__':
     setup()
+    update()
     mainloop()
 
 
