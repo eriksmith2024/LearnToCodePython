27bdd86b6a2e26a8b2234ea300b521efd4a677ca
Erik Smith <eriksmith.career@hotmail.com>
2025-11-29 20:10:27 +0000

    dog objects update

--- ch12/dog.py
+++ ch12/dog.py
@@ -25,4 +25,13 @@ class ServiceDog(Dog):
         Dog.__init__(self, name, age, weight)
         self.handler = handler
 
+def walk(self):
+    print(self.name, 'is helping its handler', self.handler, 'walk')
 
+rody = ServiceDog('Rody', 8, 38, 'Joseph')
+print("This dog's name is", rody.name)
+print("This dog's handlers name is", rody.handler)
+print_dog(rody)
+rody.bark()
+rody.walk()
+    
