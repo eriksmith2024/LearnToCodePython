e3fd3ee674991ade734c90e7e4d933ba91ddddac
Erik Smith <eriksmith.career@hotmail.com>
2025-11-29 20:03:42 +0000

    dog objects

--- ch11/tempCodeRunnerFile.py
+++ ch11/tempCodeRunnerFile.py
@@ -1,1 +1,15 @@
-   update()
+my_grid = [[0, 1, 2, 3],
+           [4, 5, 6, 7],
+           [8, 9, 10, 11]
+           ]
+
+value = my_grid[2][3]
+print(value)
+
+height = 100
+width = 100
+
+grid_model = [0] * height
+
+for i in range(height):
+    grid_model[i] = [0] * width
---
+++ ch12/dog.py
@@ -0,0 +1,28 @@
+class Dog:
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+    
+    def bark(self):
+        if self.weight > 29:
+            print(self.name, 'says "WOOF WOOF"')
+        else:
+            print(self.name, 'says "woof woof"')
+
+def print_dog(dog):
+    print(dog.name + "'s", 'age is', dog.age,
+                           'and weight is', dog.weight)
+
+codie = Dog('Codie', 12, 38)
+jackson = Dog('Jackson', 9, 12)
+print_dog(codie)
+print_dog(jackson)
+
+
+class ServiceDog(Dog):
+    def __init__(self, name, age, wight, handler):
+        Dog.__init__(self, name, age, weight)
+        self.handler = handler
+
+
