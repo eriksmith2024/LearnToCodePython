f9a94f5659851b39a5505e4a6a12ad2971e23f12
Erik Smith <eriksmith.career@hotmail.com>
2025-12-14 10:48:33 +0000

    list comprehension updates

--- List Comprehensions/listComprehensions.py
+++ List Comprehensions/listComprehensions.py
@@ -1,2 +1,8 @@
 
 print([x + x for x in range (10)])
+
+lyric = ['I', 'saw', 'heard', 'on', 'you', 'the', 'wireless', 'back', 'in', '52',]
+
+print([s[0]for s in lyric])
+
+print([s[0] for s in lyric if s[0] > 'm'])
